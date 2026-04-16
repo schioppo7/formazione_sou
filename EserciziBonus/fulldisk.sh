0e658182329f7596042c90bda42e4ea9b3ac93ea
schioppolone <davideulizi@libero.it>
2026-04-16 16:26:01 +0200

    Fill disk

--- EserciziBonus/fulldisk.sh
+++ EserciziBonus/fulldisk.sh
@@ -19,18 +19,22 @@ fi
 #!/bin/bash
 
 DIRECTORY="/home/davide/test_esplosione"
-NOME_FILE="$DIRECTORY/file_corrotto.dat"
 mkdir -p "$DIRECTORY"
 
 echo
 echo "inizio procedura di implosione"
 
-if ! dd if=/dev/urandom of="$NOME_FILE" bs=1M count = 1000  2> /dev/null
-then
-    echo "DISCO PIENO!!!!!"
-    exit 1
-fi
+for ((i=1; i<=30; i++))
+do
+	NOME_FILE="$DIRECTORY/file_corrotto$i.dat"
+	dd if =/dev/urandom of="$NOME_FILE" bs=1M count=5000 2> dev/null
 
+	STATO=$?
+	if [ $STATO -ne 0]; then
+    		echo "DISCO PIENO!!!!! Codice: "$STATO"
+    		exit 1
+	fi
+done
 
 
 
